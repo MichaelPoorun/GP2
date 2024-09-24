2b28c99dd985da39f22fca27993a04452fb5bb0c
MichaelPoorun <102557175+MichaelPoorun@users.noreply.github.com>
2024-09-23 23:27:14 -0400

    Setting Up Repository and 1st Project

---
+++ GP2-Project 1/Assets/Script/Enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health, maxHealth = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damageAmount) 
+    {
+        health -= damageAmount;
+
+        if (health <= 0 ) 
+        {
+            GameManager.score++;
+            Destroy(gameObject);
+        }
+    }
+}
